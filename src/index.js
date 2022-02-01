4db8e98dcd5f6275ecd800339322f9b2622ce973
Benjamin Semah <benjaminsemah@gmail.com>
2022-02-01 04:28:06 +0000

    Split JavaScript code into modules.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+import { renderScore, addScore } from './modules/add-render.js';
+
+const submitBtn = document.querySelector('#submit-btn');
+
+submitBtn.addEventListener('click', () => {
+    addScore();
+    renderScore();
+})
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
