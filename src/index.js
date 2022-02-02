a0fd70dba48e7299d9975288eb02abbd851d8974
Benjamin Semah <benjaminsemah@gmail.com>
2022-02-02 20:59:30 +0000

    Fix errors spotted by linters

--- src/index.js
+++ src/index.js
@@ -1,35 +1,29 @@
 import './style.css';
-import { addScores , getScores, userInput, scoreInput } from './modules/lead-api.js';
+import {
+  addScores, getScores, userInput, scoreInput,
+} from './modules/lead-api.js';
 
-const submitBtn = document.querySelector("#submit-btn");
+const submitBtn = document.querySelector('#submit-btn');
 const failAlert = document.querySelector('.fail-alert');
 const refreshBtn = document.querySelector('#refresh-btn');
 
 window.addEventListener('pageshow', () => {
   getScores();
-})
+});
 
 refreshBtn.addEventListener('click', () => {
   getScores();
-})
+});
 
-submitBtn.addEventListener("click", () => {
-  console.log('Submit');
-  if (
-    isNaN(scoreInput.value) === true ||
-    scoreInput.value === "" ||
-    userInput.value === ""
-  ) {
-    console.log('Fail');
+submitBtn.addEventListener('click', () => {
+  if (scoreInput.value === '' || userInput.value === '') {
     scoreInput.value = '';
-    failAlert.innerHTML = 'Submission failed. Try again.'
+    failAlert.innerHTML = 'Submission failed. Try again.';
   } else {
-    console.log('Pass');
     addScores();
   }
 });
 
 scoreInput.addEventListener('click', () => {
-  console.log('ScoreInput');
   failAlert.innerHTML = '';
-})
+});
--- src/modules/lead-api.js
+++ src/modules/lead-api.js
@@ -1,39 +1,39 @@
-export const userInput = document.querySelector(".name-input");
-export const scoreInput = document.querySelector(".score-input");
+export const userInput = document.querySelector('.name-input');
+export const scoreInput = document.querySelector('.score-input');
 const scoreBoard = document.querySelector('#score-board');
 
-const requestURL = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BHoD0BPYjAhBYV6sxpn7/scores/";
+const requestURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1FO2b9q2LcMXOmf0R1EU/scores/';
 
 export async function getScores() {
   const response = await fetch(requestURL);
   const scoresArr = await response.json();
   const devScores = scoresArr.result;
 
-  let scoreHTML = ``;
-  devScores.forEach(player => {
-    scoreHTML +=
-    `
+  let scoreHTML = '';
+  devScores.forEach((player) => {
+    scoreHTML
+    += `
     <li class="single-score">
       <p class="name">${player.user}</p>
       <p class="score">${player.score}</p>
     </li>
-    ` 
-  })
+    `;
+  });
   scoreBoard.innerHTML = scoreHTML;
-};  
+}
 
 export async function addScores() {
-  const response = await fetch(requestURL, {
-    method: "POST",
+  await fetch(requestURL, {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(
       {
         user: userInput.value.trim(),
-        score: scoreInput.value.trim()
-      }
-    )
+        score: scoreInput.value.trim(),
+      },
+    ),
   });
   userInput.value = '';
   scoreInput.value = '';
